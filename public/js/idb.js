78af1007e6620dff655c7f0a99157c844de745b2
Varsha Jain <csvarshajain@gmail.com>
2022-01-03 23:14:24 -0500

    added service worker

--- public/js/idb.js
+++ public/js/idb.js
@@ -19,7 +19,7 @@ request.onsuccess = function (event) {
     // check if app is online, if yes run uploadBudget() function to send all local db data to api
     if (navigator.onLine) {
         // we haven't created this yet, but we will soon, so let's comment it out for now
-        // uploadBudget();
+        uploadBudget();
     }
 };
 
@@ -33,9 +33,58 @@ function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_budget'], 'readwrite');
 
-    // access the object store for `new_pizza`
+    // access the object store for new_budget`
     const budgetObjectStore = transaction.objectStore('new_budget');
 
     // add record to your store with add method
     budgetObjectStore.add(record);
 }
+
+function uploadBudget() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+    // upon a successful .getAll() execution, run this function
+
+    getAll.onsuccess = function () {
+        // also send to server
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => {
+                    return response.json();
+                })
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['new_budget'], 'readwrite');
+                    // access the new_budget object store
+                    const budgetObjectStore = transaction.objectStore('new_budget');
+                    // clear all items in your store
+                    budgetObjectStore.clear();
+
+                    alert('All saved data has been submitted!');
+                })
+                .catch(err => {
+                    console.log(err)
+
+                });
+        }
+    }
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadBudget)
